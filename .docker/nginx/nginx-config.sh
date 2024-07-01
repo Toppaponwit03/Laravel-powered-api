158a7fa238495feaca91e2d40a78df910dcd18df
Toppaponwit03 <toppaponwit1996@hotmail.com>
2024-07-01 10:09:22 +0700

    update

--- .docker/nginx/nginx-config.sh
+++ .docker/nginx/nginx-config.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-set -e  # Exit immediately if a command exits with a non-zero status
-set -u  # Treat unset variables as an error and exit immediately
-
-
-# Debugging output
-echo "Port is: ${NGINX_PORT}"
-
-# Check if the template file exists
-if [ ! -f /etc/nginx/conf.d/default.conf.template ]; then
-  echo "Template file /etc/nginx/conf.d/default.conf.template not found!"
-  exit 1
-fi
+# echo 'Port is :' ${NGINX_PORT}
 
 # Substituting the environment variables in the template file and writing to the actual config file
 envsubst '${NGINX_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Validate the new NGINX configuration
-nginx -t
 
 # Executing the command passed to the script (default is to start nginx)
 exec "$@"
