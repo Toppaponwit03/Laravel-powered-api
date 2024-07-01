665cacc21dd31a3c52abd0bd98855313ee7ebbb4
Toppaponwit03 <toppaponwit1996@hotmail.com>
2024-07-01 13:43:14 +0700

    update Docker

--- .docker/nginx/nginx-config.sh
+++ .docker/nginx/nginx-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# echo 'Port is :' ${NGINX_PORT}
+echo 'Port is :' ${NGINX_PORT}
 
 # Substituting the environment variables in the template file and writing to the actual config file
 envsubst '${NGINX_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
